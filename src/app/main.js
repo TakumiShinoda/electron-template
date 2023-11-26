503769c835ea4d263f8a489570ad333781009e48
unknown <godgodx4@gmail.com>
2023-11-27 02:31:29 +0900

    update examples

--- src/app/main.js
+++ src/app/main.js
@@ -1,22 +1,31 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, ipcMain, dialog} = require('electron');
 const electronReload = require('electron-reload')('./dist/');
 
 const {distPath} = require('../../dev/path');
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
-    width: 640,
-    height: 480,
+    width: 960,
+    height: 770,
     resizable: true,
     movable: true,
-    'webPreferences': {
-      'webviewTag': true
+    webPreferences: {
+      webviewTag: true,
+      preload: `${__dirname}/preload.js`
     }
     // transparent: true,
     // titleBarStyle: 'hidden',
     // frame: false,
   });
   mainWindow.loadURL('file://' + distPath.views('/index/index.html'));
+
+  ipcMain.on('openDialog', async () => {
+    dialog.showMessageBox(mainWindow, {message: 'Dialog by main process.'})
+  })
+
+  ipcMain.handle('openFileDialog', async (_, filters) => {
+    return await dialog.showOpenDialogSync(mainWindow, { filters: filters ,properties: ['openFile'] });
+  })
 
   mainWindow.on('closed', () => {
     mainWindow = null;
--- src/assets/typescript/index/index.ts
+++ src/assets/typescript/index/index.ts
@@ -1,18 +1,41 @@
-import Test from './module'
-
 import '../../css/index/styles.css'
 
 $(function (){
-  let webviewExm1: HTMLElement | any = $('#webviewExm1')[0]; 
-  let module: Test = new Test() 
-
-  module.logMes('hoge')
+  let webviewExm1: HTMLElement | any = $('#webviewExm1')[0]
+  let modal: any = $('#modal')
 
   webviewExm1.addEventListener('dom-ready', () => {
-    webviewExm1.openDevTools();
-  });
+    $('#openDevToolButton').on('click', () => {webviewExm1.openDevTools()})
+  })
 
   addEventListener('beforeunload', () => {
-    webviewExm1.closeDevTools();
-  });
-});
+    webviewExm1.closeDevTools()
+  })
+
+  $('#openModalButton').on('click', () => {
+    modal.modal('show')
+  })
+
+  $('#onewayIpcButton').on('click', () => {
+    (window as any).electronAPI.openDialog()
+  })
+
+  $('#withRespIpcButton').on('click', async () => {
+    let dialogFilter: {extensions: string[], name: string}[] = [
+      {
+        extensions: ['*jpg'], 
+        name: 'JPEG'
+      }
+    ]
+    let windowAsAny: any = window as any
+    let paths: string
+    
+    try{
+      paths = await windowAsAny.electronAPI.openFileDialog(dialogFilter)
+      console.log(paths[0])
+      $('#directoryInput').val(paths[0])
+    }catch(err){
+      console.log(err)
+    }
+  })
+})
