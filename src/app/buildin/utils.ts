6eb4e0bbffc5f468f949c0283edc810c077f0b89
unknown <godgodx4@gmail.com>
2026-03-30 00:48:43 +0900

    add some utils

--- src/app/buildin/utils.ts
+++ src/app/buildin/utils.ts
@@ -1,14 +1,16 @@
+import * as fs from 'fs'
 import * as fsp from 'fs/promises'
 import path from 'path'
+import crypto, { BinaryLike } from 'crypto'
 
-export async function isExistFile(filePath: string){
+export async function isExistFile(filePath: string): Promise<boolean>{
   try{
     await fsp.access(filePath, fsp.constants.F_OK)
     return true
   }catch(_){return false}
 }
 
-export async function isWritableFile(filePath: string){
+export async function isWritableFile(filePath: string): Promise<boolean>{
   let targetDir: string
 
   try{
@@ -21,4 +23,72 @@ export async function isWritableFile(filePath: string){
     
     return true
   }catch(_){return false}
+}
+
+export async function isExistFolder(dirPath: string): Promise<boolean>{
+  try{
+    await fsp.access(dirPath, fsp.constants.F_OK)
+    return true
+  }catch(_){return false}
+}
+
+export async function getAllFilesRecursive(dirPath: string): Promise<string[]>{
+  let entries: fs.Dirent[]
+  let resolvedDirPath: string = path.resolve(dirPath)
+  let nextPathBuff: string
+  let result: string[] = []
+  
+  if(!await isExistFolder(resolvedDirPath)) return []
+
+  entries = await fsp.readdir(resolvedDirPath, {withFileTypes: true})
+
+  for(let e of entries){
+    nextPathBuff = `${e.path}/${e.name}`.replaceAll('\\', '/')
+
+    console.log(nextPathBuff)
+
+    if(e.isFile()) result.push(nextPathBuff)
+    else result = result.concat(await getAllFilesRecursive(nextPathBuff))
+  }
+
+  return result
+}
+
+export async function regenerateFile(filePath: string){
+  if(await isExistFile(filePath)) await fsp.unlink(filePath)
+
+  await fsp.writeFile(filePath, '')
+}
+
+export async function regenerateFolder(dirPath: string){
+  if(await isExistFolder(dirPath)) await fsp.rmdir(dirPath, {recursive: true})
+
+  await fsp.mkdir(dirPath)
+}
+
+export async function getFileHash(filePath: string, loadBytes: number = 1024 * 1024 * 100): Promise<string | undefined>{
+  const MaxLoadBytes: number = 1024 * 1024 * 100
+
+  return new Promise(async (res, _) => {
+    let dataStream: fs.ReadStream
+    let hash: crypto.Hash = crypto.createHash('sha256')
+
+    try{
+      if(!isExistFile(filePath)) throw Error('Directory not found')
+
+      if((loadBytes <= 0) || (loadBytes > MaxLoadBytes)) loadBytes = MaxLoadBytes
+
+      dataStream = fs.createReadStream(filePath, {highWaterMark: loadBytes})
+
+      dataStream.on('data', (data: BinaryLike) => {hash.update(data)})
+      dataStream.on('end', () => res(hash.digest('hex')))
+      dataStream.on('error', (_) => {res(undefined)})
+    }catch(_){res(undefined)}
+  })
+}
+
+export async function wait(millis: number): Promise<void>{
+  return new Promise((res) => {
+    setTimeout(() => {res()}, millis)
+  })
 }
