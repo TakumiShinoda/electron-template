d0ca3275257ba2575e9f417265a00b9c2867b75e
unknown <godgodx4@gmail.com>
2026-04-04 19:35:11 +0900

    fix entry point

--- src/app/main.ts
+++ src/app/main.ts
@@ -8,51 +8,53 @@ import { codeToHtmlWrap } from './ipcMain/builtin/shikiWraper'
 import { setupSecondInstanceLock } from './secondInstanceLock'
 import { setupTray } from './tray'
 
-setupSecondInstanceLock()
+(async () => {
+  setupSecondInstanceLock()
 
-app.on('ready', () => {
-  MainWindow = new BrowserWindow({
-    width: 720,
-    height: 480,
-    minWidth: 720,
-    minHeight: 480,
-    resizable: true,
-    movable: true,
-    webPreferences: {
-      webviewTag: true,
-      preload: distPath.preload('/index.js'),
-    },
-    frame: false,
-    show: false
+  app.on('ready', () => {
+    MainWindow = new BrowserWindow({
+      width: 720,
+      height: 480,
+      minWidth: 720,
+      minHeight: 480,
+      resizable: true,
+      movable: true,
+      webPreferences: {
+        webviewTag: true,
+        preload: distPath.preload('/index.js'),
+      },
+      frame: false,
+      show: false
+    })
+
+    MainWindow.loadURL(`${distPath.views('/index/index.html')}`)
+
+    MainWindow.once('ready-to-show', () => {
+      setTimeout(() => {
+        if(MainWindow == undefined) return
+
+        MainWindow.show()
+      }, 1000)
+    })
+
+    ipcMain.handle('openFileDialog', openFileDialog)
+    ipcMain.handle('shikiCodeToHtml', codeToHtmlWrap)
+    ipcMain.handle('getSrcFromDist', getSrcFromDist)
+    ipcMain.on('openDialog', openDialog)
+    ipcMain.on('maximizeWindow', maximizeWindow)
+    ipcMain.on('minimizeWindow', minimizeWindow)
+    ipcMain.on('closeWindow', closeWindow)
+
+    MainWindow.on('closed', () => {
+      MainWindow = undefined
+
+      MainTray?.destroy()
+      MainTray = undefined
+      
+      console.log('exitApp')
+      process.exit(0)
+    })
+
+    setupTray()
   })
-
-  MainWindow.loadURL(`${distPath.views('/index/index.html')}`)
-
-  MainWindow.once('ready-to-show', () => {
-    setTimeout(() => {
-      if(MainWindow == undefined) return
-
-      MainWindow.show()
-    }, 1000)
-  })
-
-  ipcMain.handle('openFileDialog', openFileDialog)
-  ipcMain.handle('shikiCodeToHtml', codeToHtmlWrap)
-  ipcMain.handle('getSrcFromDist', getSrcFromDist)
-  ipcMain.on('openDialog', openDialog)
-  ipcMain.on('maximizeWindow', maximizeWindow)
-  ipcMain.on('minimizeWindow', minimizeWindow)
-  ipcMain.on('closeWindow', closeWindow)
-
-  MainWindow.on('closed', () => {
-    MainWindow = undefined
-
-    MainTray?.destroy()
-    MainTray = undefined
-    
-    console.log('exitApp')
-    process.exit(0)
-  })
-
-  setupTray()
-})
+})()
